c7312259c033f35c4d044e7093155a27f680e6a6
Sangtriever <dltkdgus1850@gmail.com>
2023-01-07 23:23:41 +0900

    1

--- src/main/java/com/test/cicdtest/Sample.java
+++ src/main/java/com/test/cicdtest/Sample.java
@@ -1,14 +1,16 @@
 package com.test.cicdtest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Sample {
 
-    @RequestMapping("/sample")
-    public String greeting(){
-        return "sample!!";
+    @GetMapping("/shop")
+    public ModelAndView shop() {
+        return new ModelAndView("index");
     }
 
 }
